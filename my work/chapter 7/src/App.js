1dd34d128f334773c9e6d28dbbfb3c575cb10789
Lindelwe <lourdeslindelwe@gmail.com>
2024-05-17 11:21:47 +0200

    new chapter

---
+++ my work/chapter 7/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Products from './Products';
+import UserForm from './UserForm';
+//import JumboTronComponent from './JumboTronComponents';
+import GitHub from './GitHub';
+
+
+function App() {
+  return (
+    <div>
+    <GitHub /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ my work/chapter 7/src/GitHub.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+class GitHub extends Component {
+
+    constructor() {
+        super();
+        this.getGitHubData('greg');
+    }
+    getGitHubData(_searchTerm) {
+        axios.get("https://api.github.com/search/users?q=" + _searchTerm)
+            .then(res => {
+                console.log(res.data.items);
+            });
+    }
+    render() {
+        return (
+            <div>
+                check your console
+            </div>
+        );
+    }
+}
+export default GitHub;
